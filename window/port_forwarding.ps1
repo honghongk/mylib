225a060bd1f15543c8a96d4af36d6fb641144a69
honghongk <40760558+honghongk@users.noreply.github.com>
2022-08-10 16:28:43 +0900

    Create port_forwarding.ps1

---
+++ window/port_forwarding.ps1
@@ -0,0 +1,39 @@
+# 어디선가 복사해온거
+# wsl로 연결되는 아이피가 컴퓨터 껏다 킬때마다 계속바뀌어서 따로 실행,
+# 작업 스케줄러에 추가  트리거는 모든사용자 로그온시
+
+# Start-Sleep -Seconds 120
+$remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
+$found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+
+if( $found ){
+  $remoteport = $matches[0];
+} else{
+  echo "The Script Exited, the ip address of WSL 2 cannot be found";
+  exit;
+}
+
+#[Ports]
+
+#All the ports you want to forward separated by coma
+$ports=@(80);
+
+
+#[Static ip]
+#You can change the addr to your ip config to listen to a specific address
+$addr='0.0.0.0';
+$ports_a = $ports -join ",";
+
+
+#Remove Firewall Exception Rules
+iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
+
+#adding Exception Rules for inbound and outbound Rules
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+
+for( $i = 0; $i -lt $ports.length; $i++ ){
+  $port = $ports[$i];
+  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
+  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+}
