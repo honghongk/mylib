3bc1e019b9887f7ecb4163e0dd4a438d1884c381
honghongk <40760558+honghongk@users.noreply.github.com>
2022-08-10 16:11:59 +0900

    Update Arr.php

--- php/Arr.php
+++ php/Arr.php
@@ -1,47 +1,88 @@
 <?php
 
+
+
+/**
+ * 배열 관련 함수 모음
+ */
 class Arr
 {
-	/**
-	 * 다차원 배열을 1차원으로 변환
-	 * $arr array
-	 */
-	static function flat ( $arr )
-	{
-		if (!is_array($arr))
-			return $arr;
-		$res = array();
-		foreach ($arr as $k => $v)
-		{
-			if ( is_array ( $v ) )
-				$res = array_merge ( $res , self::flat ( $v ) ) ;
-			else
-				$res[$k] = $v;
-		}
 
-		return $res;
-	}
+    /**
+     * k => v 인지 확인
+     * @param array 배열
+     * @return bool
+     */
+    static function isAssoc($arr)
+    {
+        return ! empty ($arr) && array_values($arr) !== $arr;
+    }
 
 
-	/**
-	 * 다차원 배열 순회 array_walk_recursive랑 다름
-	 */
-	static function loop ( $arr , $closure )
-	{
-		if ( ! is_array ( $arr ) )
-			return FALSE ;
-		foreach ( $arr as $k => $v )
-		{
-			$closure ( $k , $v ) ;
-			if ( is_array ( $v ) )
-				self::loop ( $v , $closure ) ;
-		}
-	}
+    /**
+     * k => v 아닌지 확인
+     * @param array 배열
+     * @return bool
+     */
+    static function isList($arr)
+    {
+        // 8.1 이상
+        // return array_is_list($arr);
+        return ! empty ($arr) && ! self::isAssoc($arr);
+    }
 
 
-	/**
-	 * 배열의 깊이 얻기
-	 */
+    /**
+     * 다차원 배열 1차로 펴기
+     * @param array 배열
+     * @return array 1차로 펴진 배열
+     */
+    static function flat ( $arr )
+    {
+        if (!is_array($arr))
+                return $arr;
+        $res = array();
+        foreach ($arr as $k => $v)
+        {
+            if ( is_array ( $v ) )
+                    $res = array_merge ( $res , self::flat ( $v ) ) ;
+            else
+                    $res[$k] = $v;
+        }
+
+        return $res;
+    }
+
+
+    /**
+     * https://stackoverflow.com/questions/8392619/php-function-to-get-recursive-path-keys-with-path
+     * @param iterable 배열, 오브젝트
+     * @param callable 함수
+     * @param array 경로
+     * @return array 모든 값 경로 1차
+     */
+    static function recursive ( $arr , callable $act, $path = [] )
+    {
+        $res = array();
+        foreach ($arr as $k => $v)
+        {
+            $current = array_merge($path, array($k));
+            $act($k,$v,$current);
+            if (is_array($v) || is_object($v))
+                $res = array_merge($res, self::recursive($v, $act , $current));
+            else
+                $res[] = join('_', $current);
+        }
+        return $res;
+    }
+
+
+    /**
+     * 배열 깊이 구하기
+     * 몇차원 배열인지 구하기
+     * @param array 배열
+     * @return int 배열 깊이
+     */
 	static function depth ( $arr )
 	{
 		if ( ! is_array ( $arr ) )
