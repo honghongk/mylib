355b534ff3f7bdbb25860f572f438565f96d335c
honghongk <40760558+honghongk@users.noreply.github.com>
2023-03-10 11:33:30 +0900

    Update k3s.sh

--- shell/install/k3s.sh
+++ shell/install/k3s.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+
+# centos 7 기준
  
  yum update -y
  yum install -y epel-release
 # https://www.linkedin.com/pulse/install-kubernetes-using-k3s-centos7-prayag-sangode
 
-# centos 7 기준
 curl -sfL https://get.k3s.io | sh -
 
+# 도커 설치
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum -y install docker-ce
