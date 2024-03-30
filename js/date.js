aa0d1e023d600b07b73dfd5cd8ee750038d1a0c4
honghongk <40760558+honghongk@users.noreply.github.com>
2024-03-30 18:49:43 +0900

    Update date.js

--- js/date.js
+++ js/date.js
@@ -11,18 +11,40 @@ function date_format(date = new Date(), format = 'Y-m-d H:i:s')
         date = new Date(Number(date));
     else if (typeof date == 'string')
         date = new Date(date);
+
+    // 파싱 불가 에러
+    if ( date == 'Invalid Date' )
+        throw date;
+
     let formating = {
+        y : String(date.getFullYear()).substr(2,2),
         Y : date.getFullYear(),
         m : date.getMonth() + 1,
         d : date.getDate(),
         H : date.getHours(),
         i : date.getMinutes(),
-        s : date.getSeconds()
+        s : date.getSeconds(),
+        w : function(){
+            let day = ['일','월','화','수','목','금','토'];
+            return day[date.getDay()];
+        },
+
+        // 영어로는 am pm 보내야함
+        A : function(date){
+            let h = date.getHours()
+            if ( h < 12 )
+                return '오전';
+            return '오후';
+        },
     }
 
     for (const key in formating)
     {
-        let v = formating[key];
+        let v = '';
+        if ( formating[key] instanceof Function)
+            v = formating[key](date);
+        else
+            v = formating[key];
         if ( v < 10 )
             v = "0" + String(v)
         format = format.replace(key, v);
